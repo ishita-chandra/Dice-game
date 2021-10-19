cefdf48f03d0205f4b85c01d172e5ba29d04855c
ishita-chandra <69865254+ishita-chandra@users.noreply.github.com>
2021-10-19 22:45:02 +0530

    Initial dice game

---
+++ index.js
@@ -0,0 +1,62 @@
+var n1=prompt("Enter the name of Player 1");
+var n11=n1;
+var n2=prompt("Enter the name of Player 2");
+var n22=n2;
+n1=(n1.slice(0,1)).toUpperCase()+(n11.slice(1,n11.length));
+n2=(n2.slice(0,1)).toUpperCase()+(n22.slice(1,n22.length));
+document.querySelector(".dice1 .player").innerHTML=n1;
+document.querySelector(".dice2 .player").innerHTML=n2;
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var b=document.querySelector(".img1");
+
+if(randomNumber1===1){
+b.setAttribute("src","images/dice1.png");
+}
+else if(randomNumber1===2){
+b.setAttribute("src","images/dice2.png");
+}
+else if(randomNumber1===3){
+b.setAttribute("src","images/dice3.png");
+}
+else if(randomNumber1===4){
+b.setAttribute("src","images/dice4.png");
+}
+else if(randomNumber1===5){
+b.setAttribute("src","images/dice5.png");
+}
+else {
+  b.setAttribute("src","images/dice6.png");
+}
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var c=document.querySelector(".img2");
+
+if(randomNumber2===1){
+c.setAttribute("src","images/dice1.png");
+}
+else if(randomNumber2===2){
+c.setAttribute("src","images/dice2.png");
+}
+else if(randomNumber2===3){
+c.setAttribute("src","images/dice3.png");
+}
+else if(randomNumber2===4){
+c.setAttribute("src","images/dice4.png");
+}
+else if(randomNumber2===5){
+c.setAttribute("src","images/dice5.png");
+}
+else {
+  c.setAttribute("src","images/dice6.png");
+}
+if(randomNumber1>randomNumber2)
+{
+  document.querySelector(".moto").innerHTML="🚩"+n1+" wins!";
+}
+else if(randomNumber1<randomNumber2)
+{
+  document.querySelector(".moto").innerHTML=n2+" wins!🚩";
+}
+else
+{
+  document.querySelector(".moto").innerHTML="🚩It's a Draw!🚩";
+}
